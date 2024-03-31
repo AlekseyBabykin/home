bda4f97f9c50064d2d3de93b56b06b3a63bbd876
Aleksey <a.babykiny@gmail.com>
2024-03-31 19:40:12 +0300

    exam

---
+++ codio-exercises-pdf/avi/script.js
@@ -0,0 +1,17 @@
+const myInput = document.getElementById("myInput");
+const myButton = document.getElementById("myButton");
+
+function isValidHexColor(color) {
+  const regex = /^#([0-9A-F]{3}){1,2}$/i;
+  return regex.test(color);
+}
+
+myButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  const inputValue = myInput.value;
+  if (isValidHexColor(inputValue)) {
+    document.body.style.backgroundColor = inputValue;
+  } else {
+    window.alert("is not correct");
+  }
+});
